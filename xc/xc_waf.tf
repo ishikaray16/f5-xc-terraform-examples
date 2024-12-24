a64571755b7382afbfb48329d39a61f881f72e57
Ishika RAY <i.ray@f5.com>
2024-12-24 19:24:37 +0530

    added created_at xc/vk8s.tf

--- xc/xc_waf.tf
+++ xc/xc_waf.tf
@@ -15,11 +15,11 @@ resource "volterra_app_firewall" "waap-tf" {
   // One of the arguments from this list "use_default_blocking_page blocking_page" must be set
   use_default_blocking_page = true
   // One of the arguments from this list "default_bot_setting bot_protection_setting" must be set
-  # default_bot_setting = true
+  default_bot_setting = true
   // One of the arguments from this list "default_detection_settings detection_settings" must be set
   default_detection_settings = true
   // One of the arguments from this list "use_loadbalancer_setting blocking monitoring" must be set
-  # use_loadbalancer_setting = true
+  use_loadbalancer_setting = true
   // Blocking mode - optional - if not set, policy is in MONITORING
   blocking = var.xc_waf_blocking
 }
